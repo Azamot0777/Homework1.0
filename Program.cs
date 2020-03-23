3ed5120a94a20e34afcb5e374316f743c2bb8e12
Heisenberg <Persey_98@list.ru>
2020-03-23 12:24:39 +0500

    Homework

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Homework1_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("1) ЗАДАЧА______ ");
+            // float a = 16.80f;
+            // float b = 12.40f;
+            // Console.WriteLine($"a = {a}; b = {b}; сред.геомет.= {Math.Sqrt(a * b)}");
+
+            // Console.WriteLine("2) ЗАДАЧА______");
+            // decimal A = 1.40m;
+            // decimal B = -5.50m;
+            // decimal C = 0.60m;
+            // Console.WriteLine($"A = {A}; B = {B}; C = {C}; AC = {A - C}; BC = {Math.Abs(B - C)}; AC + BC = {(A - C) + Math.Abs((B - C))}");
+
+            // Console.WriteLine("3) ЗАЧАЧА______");
+            // double x1  = -6.20, x2 = 2.10, y1 = 5.20, y2 = 9.80;
+            // double d = Math.Sqrt (Math.Pow((x2 - x1),2) + Math.Pow ((y2 - y1),2));
+            // Console.WriteLine($"d = {d}");
+
+            // Console.WriteLine("4) ЗАДАЧА_____");
+            // int a = 41 , b = a / 10 , c = a % 10;
+            // int d = c * 10 + b;
+            // Console.WriteLine ($"a = {a}; d = {d}");
+
+            // Console.WriteLine("5) ЗАДАЧА_______");
+            // int n = 10985;
+            // Console.WriteLine($"h = {n / 60}");
+
+            // Console.WriteLine("6) ЗАДАЧА_____");
+            // int k = 202;
+            // Console.WriteLine ($"w = {k % 7}");
+
+
+
+
+        }   
+
+    }
+}
